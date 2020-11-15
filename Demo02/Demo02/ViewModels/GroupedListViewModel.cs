19fc85b32802235e34cf1a98cc25fc043176573f
GBD <Dukarevich_G@EplusE.ru>
2020-11-15 21:02:24 +0300

    Some changes.

--- Demo02/Demo02/ViewModels/GroupedListViewModel.cs
+++ Demo02/Demo02/ViewModels/GroupedListViewModel.cs
@@ -5,43 +5,45 @@ using System.Text;
 
 namespace Demo02.ViewModels
 {
-    public class GroupedListViewModel :BaseViewModel
+    public class GroupedListViewModel : BaseViewModel
     {
         private List<PersonList> _listOfPeople;
 
         public List<PersonList> ListOfPeople
         {
-            get => _listOfPeople; 
+            get => _listOfPeople;
             set => SetProperty(ref _listOfPeople, value);
         }
 
         public GroupedListViewModel()
         {
-            var slist = new PersonList
-            {
-                new Person {FirstName = "Sally", LastName = "Sampson" },
-                new Person {FirstName = "Taylor", LastName = "Swift" },
-                new Person {FirstName = "John", LastName = "Smith" }
-            };
-            slist.Heading = "Letter-S";
-
-            var dlist = new PersonList
-            {
-                new Person {FirstName = "Jane", LastName = "Doe" },
-                new Person {FirstName = "Dick", LastName = "Donald" },
-                new Person {FirstName = "Anna", LastName = "Dog" }
-            };
-            dlist.Heading = "Letter-D";
-
-            var jlist = new PersonList
-            {
-                new Person {FirstName = "Billy", LastName = "Joel" }
-            };
-            jlist.Heading = "Letter-J";
-
             var list = new List<PersonList>
             {
-                slist, dlist, jlist
+                new PersonList
+                {
+                    Heading = "Letter-S",
+                    Persons = {
+                        new Person {FirstName = "Sally", LastName = "Sampson" },
+                        new Person {FirstName = "Taylor", LastName = "Swift" },
+                        new Person {FirstName = "John", LastName = "Smith" }
+                    }
+                },
+                new PersonList
+                {
+                    Heading = "Letter-D",
+                    Persons = {
+                        new Person {FirstName = "Jane", LastName = "Doe" },
+                        new Person {FirstName = "Dick", LastName = "Donald" },
+                        new Person {FirstName = "Anna", LastName = "Dog" }
+                    }
+                },
+                new PersonList
+                {
+                    Heading = "Letter-J",
+                    Persons = {
+                        new Person {FirstName = "Billy", LastName = "Joel" }
+                    }
+                }
             };
 
             ListOfPeople = list;
