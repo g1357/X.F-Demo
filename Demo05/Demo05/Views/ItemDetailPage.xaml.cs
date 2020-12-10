2d4abccbb46d25a0fb624a86968a868b3c7827db
GBD <Dukarevich_G@EplusE.ru>
2020-12-10 15:19:32 +0300

    ...

---
+++ Demo05/Demo05/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Demo05.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Demo05.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Demo05/Demo05/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Demo05.Models;
+using Demo05.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo05.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
